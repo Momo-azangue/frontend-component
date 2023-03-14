c7ab73d00949469adfb5cb60c50ac711a1cb741b
Momo-azangue <azanguewill@gmail.com>
2023-03-14 18:39:42 +0100

    php  code

--- PhPproject/index.js
+++ PhPproject/index.js
@@ -3,16 +3,26 @@ statusTxt= form.querySelector(".button-area span");
 
 form.onsubmit =(e)=>{
     e.preventDefault(); /// preventing form from submitting
-statusTxt.style.display = "block";
+    statusTxt.style.color = "#0D6EFD";
+    statusTxt.style.display = "block";
 
 let xhr = new XMLHttpRequest();//creating new xml object
 xhr.open("POST", "message.php", true);//sending post request to message.php file
 xhr.onload = ()=>{ //once ajax loaded
-    if(xhr.readyState == 4 && xhr.stauts == 200){// if ajax response status is 200 & ready status is 4 means the is no any error
+    if(xhr.readyState == 4 && xhr.status == 200){// if ajax response status is 200 & ready status is 4 means the is no any error
         let response = xhr.response;// storing ajax response in a response variable
-        console.log(response);
+       //if response is an errror like senter valid email address then we'll change status color to  red else reset the form
+        if(response.indexOf("Email and password field is required!") != -1 || response.indexOf("Enter a valid email addresss!") || response.indexOf("Sorry, failed to send your message!")){
+            statusTxt.style.color = "red";
+        }else{
+            form.reset();
+            setTimeout(()=>{
+                statusTxt.style.display = "none";
+            }, 3000);//hide the statusTxt after 3second if the msg is sent
+        }
+        statusTxt.innerText = response;
     }
 }
 let formData = new FormData(form); // creating new FormData obj. this obj is used to send form data 
-xhr.send(formData);//sending 
+xhr.send(formData);//sending  form datas
 }
--- PhPproject/message.php
+++ PhPproject/message.php
@@ -7,5 +7,28 @@ $phone = $_POST['phone'];
 $website = $_POST['website'];
 $message = $_POST['message'];
 
-echo $name; 
+    if(!empty($email) && !empty($message)){
+        //if email and message field is not empty
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            // if user entered email is valid
+            $receiver= "azanguewill@gmail.com";
+            $subject = "From: $name <$email>";// subject of the email. subject looks like From: Momo <azanguewill@gmail.com>
+            //merging cocating all user valeurs inside boby variable. \n is used for new line 
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage: $message\n\nRegards,\n$name";
+            $sender = "Fromm: $email";//sender email
+            if(mail($receiver, $subject, $body, $sender)){ //mail() is a inbuilt php function to send mail
+                echo "Your message has been sent!";
+
+            }else{
+                echo "Sorry, failed to send your message!";
+            }
+
+        }else{
+            echo "Enter a valid email address!";
+        }
+
+    }else{
+        echo "Email and password field is required!";
+    }
+
 ?>
