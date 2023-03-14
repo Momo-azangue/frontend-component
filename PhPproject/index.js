0b08e69d0c34218a5f8cd252761d90b08c3f85db
Momo-azangue <azanguewill@gmail.com>
2023-03-14 11:05:52 +0100

    php  code

--- PhPproject/index.js
+++ PhPproject/index.js
@@ -1,6 +1,17 @@
 const form = document.querySelector("form"),
-statusTxT= form.querySelector(".button-area span");
+statusTxt= form.querySelector(".button-area span");
 
 form.onsubmit =(e)=>{
-    e.precentDefault();
+    e.preventDefault(); /// preventing form from submitting
+statusTxt.style.display = "block";
+
+let xhr= new XMLHttpRequest();//creating new xml object
+xhr.open("POST", "message.php", true);//sending post request to message.php file
+xhr.onload = ()=>{ //once ajax loaded
+    if(xhr.readyState == 4 && xhr.stauts == 200){// if ajax response status is 200 & ready status is 4 means the is no any error
+        let response = xhr.response;// storing ajax response in a response variable
+        console.log(response);
+    }
+}
+xhr.send();
 }
---
+++ PhPproject/message.php
@@ -0,0 +1,3 @@
+<?php 
+echo "this msg sent from phpp file ";
+?>
